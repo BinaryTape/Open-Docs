5d1a1b2eea753b089b18f4aaa5a1963d764a7ab8
Lowae <1538196177@qq.com>
2025-05-21 23:40:05 +0800

    feature: migrate kotlin; refactor i18n config

--- docs/.vitepress/docs.config.ts
+++ docs/.vitepress/docs.config.ts
@@ -1,15 +1,18 @@
 export type DocsItemConfig = {
+    type: "koin" | "kotlin";
     title: string;
     description: string;
     path: string;
 }
 export const DocsTypeConfig: { [key: string]: DocsItemConfig } = {
     koin: {
+        type: "koin",
         title: "Koin",
         description: "Koin",
         path: "/koin/",
     },
     kotlin: {
+        type: "kotlin",
         title: "Kotlin",
         description: "Kotlin",
         path: "/kotlin/",
