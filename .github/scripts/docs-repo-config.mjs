d6f60e7bc50f1adc29cd04a917192d2d37f8828b
Ikutsu <ikutsu.dev@gmail.com>
2025-08-04 06:32:43 +0800

    restore repository configurations for Koin and SQLDelight

--- .github/scripts/docs-repo-config.mjs
+++ .github/scripts/docs-repo-config.mjs
@@ -3,34 +3,34 @@ import { sqlDelightStrategy } from "./strategy-sqldelight.mjs";
 import {kotlinStrategy} from "./strategy-kotlin.mjs";
 
 export const REPOS = [
-  // {
-  //   name: "koin",
-  //   repo: "InsertKoinIO/koin",
-  //   branch: "origin/main",
-  //   path: "koin-repo",
-  //   lastCheckFile: ".github/last_check_koin.txt",
-  //   strategy: koinStrategy,
-  // },
-  // {
-  //   name: "koin",
-  //   repo: "InsertKoinIO/koin-annotations",
-  //   branch: "origin/main",
-  //   path: "koin-annotations-repo",
-  //   lastCheckFile: ".github/last_check_koin_annotations.txt",
-  //   strategy: koinStrategy,
-  // },
-  // {
-  //   name: "sqldelight",
-  //   repo: "sqldelight/sqldelight",
-  //   branch: "origin/master",
-  //   path: "sqldelight-repo",
-  //   lastCheckFile: ".github/last_check_sqldelight.txt",
-  //   assets: {
-  //     src: "docs/images",
-  //     dest: "docs/public/sqldelight",
-  //   },
-  //   strategy: sqlDelightStrategy,
-  // },
+  {
+    name: "koin",
+    repo: "InsertKoinIO/koin",
+    branch: "origin/main",
+    path: "koin-repo",
+    lastCheckFile: ".github/last_check_koin.txt",
+    strategy: koinStrategy,
+  },
+  {
+    name: "koin",
+    repo: "InsertKoinIO/koin-annotations",
+    branch: "origin/main",
+    path: "koin-annotations-repo",
+    lastCheckFile: ".github/last_check_koin_annotations.txt",
+    strategy: koinStrategy,
+  },
+  {
+    name: "sqldelight",
+    repo: "sqldelight/sqldelight",
+    branch: "origin/master",
+    path: "sqldelight-repo",
+    lastCheckFile: ".github/last_check_sqldelight.txt",
+    assets: {
+      src: "docs/images",
+      dest: "docs/public/sqldelight",
+    },
+    strategy: sqlDelightStrategy,
+  },
   {
     name: "kotlin",
     repo: "JetBrains/kotlin-web-site",
